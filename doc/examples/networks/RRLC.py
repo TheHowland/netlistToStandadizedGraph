40ad8ed6d909e182e43827dfbfecc72501d036ed
yannick.wieland <yannick.wieland@hs-pforzheim.de>
2025-02-27 11:16:12 +0100

    remove lcapy files

--- doc/examples/networks/RRLC.py
+++
@@ -1,4 +0,0 @@
-from lcapy import R, L, C
-
-n = R('R0') + (C('C1') | (R('R1') + L('L1')))
-n.draw('RRLC.png')
--- lcapy/schematics/utils.py
+++
@@ -1,5 +0,0 @@
-def check_boolean(value):
-
-    if value not in (True, False, 'none', None, 'true', 'false'):
-        raise ValueError('Unexpected Boolean value %s' % value)
-    return value in (True, 'true')
