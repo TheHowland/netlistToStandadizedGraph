ba3816021485ff9d5ea86db009fab7bbbc602bc2
yannick.wieland <yannick.wieland@hs-pforzheim.de>
2025-02-28 17:18:09 +0100

    moved to drawingTree

--- generalizeNetlistDrawing/testFile.py
+++ generalizeNetlistDrawing/testFile.py
@@ -1,19 +1,9 @@
-import lcapy
 from lcapy.validateCircuitFile import ValidateCircuitFile
-from lcapy import Circuit
-from lcapy import DrawWithSchemdraw
-from lcapy.langSymbols import LangSymbols
-from netlistGraph import NetlistGraph
-from netlistToGraph import NetlistToGraph
-from lcapy.solution import Solution
+from drawingTree import DrawingTree
 
 if not ValidateCircuitFile(["test1.txt"]):
     exit("File not valid")
 
-cct = Circuit("test1.txt")
-graph = NetlistToGraph(cct)
-b = NetlistGraph(graph.MultiDiGraph(), graph.startNode, graph.endNode)
-b.findParallelSubGraphs()
-print(b.subGraphs)
+a = DrawingTree()
 # lcapyCir = lcapy.Circuit("..\\Circuits\\resistor\\00_Resistor_Hetznecker.txt")
 # a = NetlistGraph(lcapyCir)
