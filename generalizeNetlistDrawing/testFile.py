84839e989008e872051e3bd734d03ea3a004ab23
Yannick Wieland <yannick.wieland@gmail.com>
2025-04-22 10:41:25 +0200

    test direction

--- generalizeNetlistDrawing/testFile.py
+++ generalizeNetlistDrawing/testFile.py
@@ -1,8 +1,20 @@
-from backends.schemdraw.draw import DrawWithSchemdraw
+# from backends.schemdraw.draw import DrawWithSchemdraw
+import schemdrawInskale as sd
 
-# fileName = "../Circuits/resistor/00_Resistor_Hetznecker.txt"
-# fileName = "test1.txt"
-# if not ValidateCircuitFile([fileName]):
-#     exit("File not valid")
+from generalizeNetlistDrawing.elements.element import Direction as d
+from generalizeNetlistDrawing.elements.sourceV import SourceV
 
-DrawWithSchemdraw("test1.txt")
+drawing = sd.Drawing(canvas='svg')
+down = SourceV(0, 0, "V1", rotation=d.down.value, scaling=3.0)
+left = SourceV(0, 0, "V2", rotation=d.left.value, scaling=3.0)
+up = SourceV(0, 0, "V3", rotation=d.up.value, scaling=3.0)
+right = SourceV(0, 0, "V4", rotation=d.right.value, scaling=3.0)
+
+drawing.add(up.schemdrawElement())
+drawing.add(left.schemdrawElement())
+drawing.add(right.schemdrawElement())
+drawing.add(down.schemdrawElement())
+
+drawing.draw()
+
+#DrawWithSchemdraw("test1.txt")
