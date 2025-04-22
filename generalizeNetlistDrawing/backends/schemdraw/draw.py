f2bd6938c3c43bd6a3bbf70d8b0b6f37d3740cf6
Yannick Wieland <yannick.wieland@gmail.com>
2025-04-22 14:30:35 +0200

    optimize for destop or mobile

--- generalizeNetlistDrawing/backends/schemdraw/draw.py
+++ generalizeNetlistDrawing/backends/schemdraw/draw.py
@@ -1,10 +1,18 @@
+from enum import Enum
+
 import schemdrawInskale as sd
 
 from generalizeNetlistDrawing.elementPlacement import ElementPlacement
 
 
+class Optimize(Enum):
+    NONE = None
+    DESKTOP = 'desktop'
+    MOBILE = 'mobile'
+
 class DrawWithSchemdraw:
-    def __init__(self, fileName):
+    def __init__(self, fileName, rotate: float=None, optimize: Optimize = Optimize.NONE):
+
         self.placedElems = ElementPlacement(fileName)
 
         self.elementLength = 3
@@ -12,23 +20,16 @@ class DrawWithSchemdraw:
         self.d = sd.Drawing(canvas='svg')
         self.length = 0
 
+        if rotate:
+            self.rotate(rotate)
+
+        if optimize.value:
+            self.optimize(optimize)
+
         for elmPos in iter(self.placedElems.elements):
             self.d.add(elmPos.schemdrawElement())
 
-        drawing2 = sd.Drawing(canvas='svg')
-        for elmPos in iter(self.placedElems.elements):
-            elmPos.rotate(90)
-            drawing2.add(elmPos.schemdrawElement())
-
-        drawing3 = sd.Drawing(canvas='svg')
-        for elmPos in iter(self.placedElems.elements):
-            elmPos.rotate(90)
-            drawing3.add(elmPos.schemdrawElement())
-
         self.d.draw()
-        drawing2.draw()
-        drawing3.draw()
-        pass
 
     def transformGridSize(self, GRID_SIZE=3):
         """
@@ -39,6 +40,45 @@ class DrawWithSchemdraw:
         """
         for elm in iter(self.placedElems.elements):
             elm.scaleSelf(GRID_SIZE)
+
+    def rotate(self, degree: float):
+        for elm in iter(self.placedElems.elements):
+            elm.rotate(degree)
+
+    def maxXMaxY(self):
+        globMaxX, globMaxY = 0, 0
+        for elm in iter(self.placedElems.elements):
+            x1, y1 = elm.startPos.asTuple
+            x2, y2 = elm.endPos.asTuple
+            maxX = max(x1, x2)
+            maxY = max(y1, y2)
+            if maxX > globMaxX:
+                globMaxX = maxX
+            if maxY > globMaxY:
+                globMaxY = maxY
+
+        return globMaxX, globMaxY
+
+    def optimize(self, optimize: Optimize):
+        if optimize == Optimize.MOBILE:
+            self.optimizeMobile()
+        elif optimize == Optimize.DESKTOP:
+            self.optimizeDesktop()
+        else:
+            return
+
+
+    def optimizeMobile(self):
+        globMaxX, globMaxY = self.maxXMaxY()
+
+        if globMaxX > globMaxY:
+            self.rotate(90)
+
+    def optimizeDesktop(self):
+        globMaxX, globMaxY = self.maxXMaxY()
+
+        if globMaxY > globMaxX:
+            self.rotate(90)
 
 if __name__ == '__main__':
     DrawWithSchemdraw("..\\..\\test1.txt")
